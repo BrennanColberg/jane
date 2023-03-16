0a7b2a03e23c54dc40d5235d70ad424261b862f3
Brennan Colberg <brennan.colberg@gmail.com>
2023-03-15 23:16:34 -0700

    imessage command & note

--- engine/jane/interfaces/imessage.py
+++ engine/jane/interfaces/imessage.py
@@ -1,17 +1,19 @@
+# this is very complicated as an interface, do later
+# (probably with separate server, once there's an API)
+
 from applescript import AppleScript
 
-applescript_code = f'''
-on run argv
-    set phoneNumber to item 1 of argv
-    set messageText to item 2 of argv
 
-    tell application "Messages"
-        set targetService to 1st service whose service type = iMessage
-        set targetBuddy to buddy phoneNumber of targetService
-        send messageText to targetBuddy
-    end tell
-end run
-'''
+def send(address: str, message: str):
+    AppleScript(f'''
+        on run argv
+            set phoneNumber to item 1 of argv
+            set messageText to item 2 of argv
 
-applescript = AppleScript(applescript_code)
-applescript.run('', 'test message')
+            tell application "Messages"
+                set targetService to 1st service whose service type = iMessage
+                set targetBuddy to buddy phoneNumber of targetService
+                send messageText to targetBuddy
+            end tell
+        end run
+    ''').run(address, message)
