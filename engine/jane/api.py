b1c0ceece77ebc856953f22d9580ff05d3baa15e
Brennan Colberg <brennan.colberg@gmail.com>
2023-03-16 07:14:50 -0700

    /step endpoint to start session

--- engine/jane/api.py
+++ engine/jane/api.py
@@ -2,14 +2,23 @@
 
 from flask import Flask, request, jsonify
 from .engine import step
+import uuid
 app = Flask(__name__)
 
 
 @app.route('/step/<session_id>', methods=['POST'])
-def step_endpoint(session_id):
+def step_id_endpoint(session_id: str):
     input_string = request.get_data(as_text=True)
     output, history = step(session_id, input_string)
     return jsonify({"output": output, "history": history, "session_id": session_id})
+
+
+@app.route('/step', methods=['POST'])
+def step_endpoint():
+    """raw /step endpoint that generates a new UUID id for the session.
+       used to start a new session"""
+    session_id = uuid.uuid4()
+    return step_id_endpoint(session_id)
 
 
 # basic ping endpoint
