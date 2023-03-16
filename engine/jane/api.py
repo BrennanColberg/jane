9e97488dc276e8e85c7935968854a02362e23be7
Brennan Colberg <brennan.colberg@gmail.com>
2023-03-15 23:24:27 -0700

    delete overcomplicated interface file stuff

---
+++ engine/jane/api.py
@@ -0,0 +1,12 @@
+# all interfaces go through an HTTP API
+# TODO implement lol
+
+
+# salvaged from old code
+#
+# def main():
+#     print('triggered cli lol')
+#     while (True):
+#         user_input = input('You: ')
+#         [jane_response] = step(user_input)
+#         print('Jane: ' + jane_response)
--- engine/jane/interfaces/cli.py
+++
@@ -1,9 +0,0 @@
-from ..engine import step
-
-
-def main():
-    print('triggered cli lol')
-    while (True):
-        user_input = input('You: ')
-        [jane_response] = step(user_input)
-        print('Jane: ' + jane_response)
