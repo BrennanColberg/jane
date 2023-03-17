6236d9b36aa0187cfe22e323717047b815dc4808
Brennan Colberg <brennan.colberg@gmail.com>
2023-03-17 08:27:13 -0700

    non-receive parts of imessage interface

---
+++ interfaces/imessage/README.md
@@ -0,0 +1,6 @@
+# iMessage interface
+
+- runs on a macOS device
+- uses (polling?) to detect new iMessages from the user
+- uses the HTTP API to "step" through Jane's engine
+- uses AppleScript to send iMessage
---
+++ interfaces/imessage/index.py
@@ -0,0 +1,26 @@
+from applescript import AppleScript
+import sqlite3
+
+connection = sqlite3.connect('~/Library/Messages/chat.db')
+
+send_imessage_applescript = AppleScript(f'''
+    on run argv
+        set phoneNumber to item 1 of argv
+        set messageText to item 2 of argv
+
+        tell application "Messages"
+            set targetService to 1st service whose service type = iMessage
+            set targetBuddy to buddy phoneNumber of targetService
+            send messageText to targetBuddy
+        end tell
+    end run
+    ''')
+
+
+def send_imessage(recipient_address, message):
+    """send iMessage"""
+    send_imessage_applescript.run(recipient_address, message)
+
+
+# receive imessage
+# (I think this means polling)
