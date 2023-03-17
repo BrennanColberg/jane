bdf09d53440a2da036fea3c661ad4c2116aaaa4f
Brennan Colberg <brennan.colberg@gmail.com>
2023-03-17 09:28:45 -0700

    basic sqlite parse attempt

--- interfaces/imessage/MySwiftProject/Package.swift
+++ interfaces/imessage/MySwiftProject/Package.swift
@@ -3,13 +3,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "MySwiftProject",
-    products: [
-        .executable(name: "MySwiftProject", targets: ["MySwiftProject"]),
-    ],
-    dependencies: [
-    ],
-    targets: [
-        .executableTarget( name: "MySwiftProject",dependencies: []),
-    ]
+  name: "MySwiftProject",
+  products: [
+    .executable(name: "MySwiftProject", targets: ["MySwiftProject"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.12.2")
+  ],
+  targets: [
+    .executableTarget( name: "MySwiftProject",dependencies: [
+      .product(name: "SQLite", package: "SQLite.swift"),
+    ]),
+  ]
 )
