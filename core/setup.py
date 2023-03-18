6ce376de459229840d67e6352d820e2352c8e5d4
Brennan Colberg <brennan.colberg@gmail.com>
2023-03-17 20:38:36 -0700

    rename engine to core, jane to src

--- core/setup.py
+++ core/setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='jane',
-    py_modules=["jane"],
+    py_modules=["src"],
     version='0.0.1',
     packages=find_packages(),
     install_requires=[],
     entry_points={
         'console_scripts': [
-            # 'jane=jane.cli:main',
-            'api=jane.api:main'
+            'api=src.api:main'
         ]
     }
 )
