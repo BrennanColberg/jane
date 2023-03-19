f356a4cbc9e4222ca5eb1a81a6ef426ffd56e857
Brennan Colberg <brennan.colberg@gmail.com>
2023-03-18 21:58:05 -0700

    /new

--- core/src/engine.py
+++ core/src/engine.py
@@ -1,4 +1,4 @@
-
+from uuid import uuid4
 from typing import Tuple
 from .history import get_history, save_history
 from .types import History
@@ -11,6 +11,9 @@ def invoke_tool():
 
 def step(session_id: str, user_input: str) -> Tuple[str, History]:
     """takes in input, puts out response/output after doing an evaluation step (including tool invocation)"""
+    if user_input.startswith("/new"):
+        user_input = user_input[5:]
+        session_id = uuid4()
     history = get_history(session_id)
     response, history = generate(user_input, history)
     # TODO loop and invoke tools if appropriate here
